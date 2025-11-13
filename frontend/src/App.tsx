2b6f709223da73b761c306d28fe6eac52eeee385
Udeshiimasha <udeshiimasha99@gmail.com>
2025-11-13 14:58:05 +0530

    Remove Map Buider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/vendor/Dashboard';
 import Reservations from './pages/vendor/Reservations';
-import MapBuilder from './pages/admin/MapBuilder';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Route path="/" element={<Navigate to="/vendor/dashboard" replace />} />
         <Route path="/vendor/dashboard" element={<Dashboard />} />
         <Route path="/vendor/reservations" element={<Reservations />} />
-        <Route path="/admin/map-builder" element={<MapBuilder />} />
       </Routes>
     </div>
   );
